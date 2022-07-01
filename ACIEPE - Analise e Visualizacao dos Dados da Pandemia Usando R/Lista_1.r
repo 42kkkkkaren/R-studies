e97d353694c893d21f78fe5414419a73589616c0
Karen Ketlyn Ferreira Barcelos <72056327+42kkkkkaren@users.noreply.github.com>
2022-06-30 21:43:22 -0300

    Create Lista_1.r

---
+++ ACIEPE - Analise e Visualizacao dos Dados da Pandemia Usando R/Lista_1.r
@@ -0,0 +1,19 @@
+##Exercício 1
+21+21
+##[1] 42
+84/2
+##[1] 42
+7*6
+##[1] 42
+2+(2*20)
+##[1] 42
+84/(1*2)
+##[1] 42
+42-21+(7*3)
+##[1] 42
+
+##Exercício 2
+install.packages("Brasil")
+library(Brasil)
+help(anova)  ||  ?anova
+help.start()
